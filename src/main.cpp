dc7fd1a17c345d33766280b9dcc30429ea154605
bvaisakh <vaisakhb12@gmail.com>
2022-09-05 18:19:43 +0530

    Sample completed

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,42 @@
+//#include <clickhouse/client.h>
+//#include "../lib/clickhouse-cpp/clickhouse/client.h"
 #include <iostream>
+#include <clickhouse/client.h>
+
+using namespace clickhouse;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+/// Initialize client connection.
+    Client client(ClientOptions().SetHost("localhost"));
+
+/// Create a table.
+    client.Execute("CREATE TABLE IF NOT EXISTS test.numbers (id UInt64, name String) ENGINE = Memory");
+
+/// Insert some values.
+    {
+        Block block;
+
+        auto id = std::make_shared<ColumnUInt64>();
+        id->Append(1);
+        id->Append(7);
+
+        auto name = std::make_shared<ColumnString>();
+        name->Append("one");
+        name->Append("seven");
+
+        block.AppendColumn("id", id);
+        block.AppendColumn("name", name);
+
+        client.Insert("test.numbers", block);
+    }
+
+/// Select values inserted in the previous step.
+    client.Select("SELECT id, name FROM test.numbers", [](const Block &block) {
+        for (size_t i = 0; i < block.GetRowCount(); ++i) {
+            std::cout << block[0]->As<ColumnUInt64>()->At(i) << " " << block[1]->As<ColumnString>()->At(i) << "\n";
+        }
+    });
+
+/// Delete table.
+    client.Execute("DROP TABLE test.numbers");
 }
