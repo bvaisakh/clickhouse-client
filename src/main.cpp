4102066aed196099f6e582a69036937dea4912d0
bvaisakh <vaisakhb12@gmail.com>
2022-09-06 14:34:16 +0530

    Added server info printing

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
-//#include <clickhouse/client.h>
-//#include "../lib/clickhouse-cpp/clickhouse/client.h"
 #include <iostream>
 #include <clickhouse/client.h>
 
 using namespace clickhouse;
+using namespace std;
 
 int main() {
 /// Initialize client connection.
-    Client client(ClientOptions().SetHost("localhost"));
+    Client client(ClientOptions().SetHost("localhost").SetPort(9100));
+
+    ServerInfo serverInfo = client.GetServerInfo();
+    cout << "Server Name: " << serverInfo.name << endl;
+    cout << "Server TimeZone: " << serverInfo.timezone << endl;
+    cout << "Display Name: " << serverInfo.display_name << endl;
+    cout << "Version Major: " << serverInfo.version_major << endl;
+    cout << "Version Minor: " << serverInfo.version_minor << endl;
+    cout << "Version Patch: " << serverInfo.version_patch << endl;
+    cout << "Revision: " << serverInfo.revision << endl;
 
 /// Create a table.
     client.Execute("CREATE TABLE IF NOT EXISTS test.numbers (id UInt64, name String) ENGINE = Memory");
